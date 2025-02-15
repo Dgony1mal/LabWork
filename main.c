84f7243210a2863fb169ed101b26cec32a46424b
Артур <155137745+Dgony1mal@users.noreply.github.com>
2025-02-15 11:35:22 +1000

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello teacher!!!");
+	double a, b, c;
+	double x1, x2, d;
+	a=5.3;
+	b=2.5;
+	c=1.1;
+	d = b*b - 4*a*c;
+	x1 = -b + sqrt(d)/(2*a);
+	x2 = -b - sqrt(d)/(2*a);
+
+	printf("x1=%lf \n", x1);
+	printf("xw=%lf \n", x2);
 	return 0;
 }
