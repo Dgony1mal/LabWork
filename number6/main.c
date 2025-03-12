112ba83afff6cd08e2b949f2d1053151d4ff6e98
Артур <155137745+Dgony1mal@users.noreply.github.com>
2025-03-12 19:58:06 +1000

    Add files via upload

---
+++ number6/main.c
@@ -0,0 +1,21 @@
+//Write a Program to check if the given number is Even or Odd.
+#include <stdio.h>
+
+void checkOddEven(int N) {
+  
+    int r = N % 2;
+
+    if (r == 0)  {
+        printf("Even");
+    }
+  
+    else  {
+        printf("Odd");
+    }
+}
+
+int main() {
+    int N = 101;
+    checkOddEven(N);
+    return 0;
+}
