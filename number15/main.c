42310fd03640c44bb8077d620f670e523ad9ce0f
Артур <155137745+Dgony1mal@users.noreply.github.com>
2025-04-02 09:09:43 +1000

    Add files via upload

---
+++ number15/main.c
@@ -0,0 +1,13 @@
+//Write a Program to concatenate two strings.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[] = "Hello "; 
+    char s2[] = "World!";
+
+    strcat(s1, s2);
+
+    printf("%s\n", s1);  
+    return 0;
+}
